b07cf1c70a6b7404a88bcdac190a16b8d559e117
Max <max.sayenko@gmail.com>
2014-02-27 11:14:09 -0800

    Initial Commit

---
+++ AwesomeMenu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AwesomeMenu
+//
+//  Created by Maksym Saienko on 2/27/14.
+//  Copyright (c) 2014 Maksym Saienko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AwesomeMenu/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AwesomeMenu
+//
+//  Created by Maksym Saienko on 2/27/14.
+//  Copyright (c) 2014 Maksym Saienko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
